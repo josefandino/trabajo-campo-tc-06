149c272d4055acc6981fa6fd97cafeaa1f4c3d65
Jose Fandiño <josefan07@hotmail.com>
2025-09-22 20:40:51 -0500

    Crear tab control

---
+++ trabajo-campo-tc-06/TabControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabajo_campo_tc_06 {
+  public partial class tb_control : Form {
+    public tb_control() {
+      InitializeComponent();
+    }
+  }
+}
