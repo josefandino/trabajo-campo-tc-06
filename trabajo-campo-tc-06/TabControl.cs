b538c36cb0b04b0aa5acd8b84c2ddf60d6f1f63f
Jose Fandiño <josefan07@hotmail.com>
2025-09-25 19:49:09 -0500

    Agregar comentario a la Clase Cliente

--- trabajo-campo-tc-06/TabControl.cs
+++ trabajo-campo-tc-06/TabControl.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace trabajo_campo_tc_06 {
 
-
   public partial class tb_control : Form {
 
+    // Definición de la clase Cliente
     public class Cliente {
       public string Nombre { get; set; }
       public string DNI { get; set; }
